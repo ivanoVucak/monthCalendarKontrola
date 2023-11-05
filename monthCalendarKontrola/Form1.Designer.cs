6d64b63a795de04fe6778098e0fa0d8369c04bd7
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-11-05 20:41:21 +0100

    Add files via upload

---
+++ monthCalendarKontrola/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace monthCalendarKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.odaberiBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(34, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // odaberiBtn
+            // 
+            this.odaberiBtn.Location = new System.Drawing.Point(67, 298);
+            this.odaberiBtn.Name = "odaberiBtn";
+            this.odaberiBtn.Size = new System.Drawing.Size(181, 25);
+            this.odaberiBtn.TabIndex = 1;
+            this.odaberiBtn.Text = "Odaberite termin";
+            this.odaberiBtn.UseVisualStyleBackColor = true;
+            this.odaberiBtn.Click += new System.EventHandler(this.odaberiBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.odaberiBtn);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button odaberiBtn;
+    }
+}
+
